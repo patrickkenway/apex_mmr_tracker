9de37d54ff4811af75edb310330b2eee4de938cd
Patrik Puskás <pusika0502@gmail.com>
2026-08-19 15:59:45 +0200

    postgres adatbazis es docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 
+from .database import engine
+
 app = FastAPI()
 
 
 @app.get("/")
 def root():
     return {"message": "MMR Tracker API works!"}
+
+
+@app.get("/db-test")
+def database_test():
+    try:
+        with engine.connect() as connection:
+            return {"database": "connected"}
+    except Exception as e:
+        return {
+            "database": "error",
+            "message": str(e),
+        }
